31cd02266ce99d8724b142613c9343e1f1387d38
Arialyson Lima <arialyson@gmail.com>
2026-05-24 08:03:09 -0300

    soma

--- testes.py
+++ testes.py
@@ -1,51 +1,6 @@
-#Questão_01
-
-for c in range(1, 101):
-    print(c, end=" ")
-
-#Questão_02
-
-for c in range(100, -1, -1):
-    print(c, end=' ')
-
-#Questão_03
-
-n1 = int(input("Valor 1: "))
-n2 = int(input("Valor 2: "))
-for c in range(n1, n2+1):
-    print(c)
-
-#Questão_04
-
 soma = 0
 
-for c in range(1, 11):
-    n = int(input("Digite um valor: "))
-    soma += n
-print(f"A soma é {soma}")
-
-#Questão_05
-
-soma = 0 
-
 for c in range(1, 6):
-    n = int(input("Valor: "))
-    if n < 10:
-        soma += n
-print(f"A soma dos valores menores que dez é {soma}")
-
-soma = 0 
-
-for c in range(1,6):
-    n = int(input("valor: "))
+    n = int(input(f"Digite o {c}º número:: "))
     soma += n
-
-print(f"A soma dos valores é {soma}")
-
-for c in range(1, 6):
-    n = int(input("Digite um número: "))
-
-n1 = int(input("Digite o primeiro valor: "))
-n2 = int(input("Digite o segundo valor: "))
-for c in range(n1, n2):
-    print(c, end=" ")
+print(f'A soma dos valores é igual a {soma}')
