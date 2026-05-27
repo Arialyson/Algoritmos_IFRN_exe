abab14e37b0ca581f84eca4c5297232548f50a98
Arialyson Lima <arialyson@gmail.com>
2026-05-27 06:00:33 -0300

    salvando

--- testes.py
+++ testes.py
@@ -4,3 +4,11 @@ for c in range(1, 6):
     n = int(input(f"Digite o {c}º número:: "))
     soma += n
 print(f'A soma dos valores é igual a {soma}')
+
+
+soma = 0 
+
+for c in range(1, 6):
+    num = int(input("Digite um valor: "))
+    soma += num
+print(f"A soma dos números é {soma}")
