961567309bb93dc1c95d372344b4c24d0a07585a
Arialyson Lima <arialyson@gmail.com>
2026-05-27 06:08:48 -0300

    adicionando listas

--- testes.py
+++ testes.py
@@ -1,14 +1,14 @@
 soma = 0
 
+# for c in range(1, 6):
+#     n = int(input(f"Digite o {c}º número: "))
+#     soma += n
+# print(f'A soma dos valores é igual a {soma}')
+
+lista = []
+
 for c in range(1, 6):
-    n = int(input(f"Digite o {c}º número:: "))
-    soma += n
-print(f'A soma dos valores é igual a {soma}')
-
-
-soma = 0 
-
-for c in range(1, 6):
-    num = int(input("Digite um valor: "))
-    soma += num
-print(f"A soma dos números é {soma}")
+    num = str(input("Digite um nome: "))
+    if num not in lista:
+        lista.append(num)
+print(lista)
